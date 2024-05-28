4bc96a4e6a8512a46931e204780fbf7350970053
josegarcia19809 <josegarcia19809@gmail.com>
2024-05-27 21:54:16 -0600

    Actualización

--- src/main/java/com/example/compra_alimentos/Controller/HomeController.java
+++ src/main/java/com/example/compra_alimentos/Controller/HomeController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
